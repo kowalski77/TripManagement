9267b27149efe1f07372a5571b042c7c15e43a80
Christian Aranda <christian.arandadiaz@ypsomed.com>
2022-12-10 18:35:05 +0100

    refactor

--- TripManagement.Domain/Common/CoordinatesExtensions.cs
+++ TripManagement.Domain/Common/CoordinatesExtensions.cs
@@ -11,17 +11,15 @@ public static class CoordinatesExtensions
 
     private static double CalculateWithHarvesineFormula(Coordinates origin, Coordinates destination)
     {
-        (var latitude, var longitude) = GetRadians(origin, destination);
-        
-        var a = (Math.Sin(latitude / 2) * Math.Sin(latitude / 2)) +
+        var latitude = (destination.Latitude - origin.Latitude).ToRadians();
+        var longitude = (destination.Longitude - origin.Longitude).ToRadians();
+
+        var value = (Math.Sin(latitude / 2) * Math.Sin(latitude / 2)) +
                 (Math.Cos(origin.Latitude.ToRadians()) * Math.Cos(destination.Latitude.ToRadians()) *
                 Math.Sin(longitude / 2) * Math.Sin(longitude / 2));
 
-        return  2 * Math.Asin(Math.Sqrt(a));
+        return  2 * Math.Asin(Math.Sqrt(value));
     }
-
-    private static (double Latitude, double Longitude) GetRadians(Coordinates origin, Coordinates destination) =>
-        ((destination.Latitude - origin.Latitude).ToRadians(), (destination.Longitude - origin.Longitude).ToRadians());
 
     private static double ToRadians(this double degrees) => degrees * (Math.PI / 180);
 }
