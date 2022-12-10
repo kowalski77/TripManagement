4c16c3c2e12aac40a5a3f6518730429e751af90b
Christian Aranda <christian.arandadiaz@ypsomed.com>
2022-12-10 15:24:44 +0100

    refactor

--- TripManagement.IntegrationTests/Trips/CreateDraftTripTests.cs
+++ TripManagement.IntegrationTests/Trips/CreateDraftTripTests.cs
@@ -6,14 +6,14 @@ using TripManagement.Application.Trips.CreateDraft;
 using TripManagement.Contracts.Models;
 using TripManagement.Domain.Common;
 
-namespace TripManagement.IntegrationTests;
+namespace TripManagement.IntegrationTests.Trips;
 
 [Collection(IntegrationTestsConstants.IntegrationTestsCollection)]
-public class CreateDraftTests
+public class CreateDraftTripTests
 {
     private readonly TestServicesFactory factory;
 
-    public CreateDraftTests(TestServicesFactory factory)
+    public CreateDraftTripTests(TestServicesFactory factory)
     {
         this.factory = factory;
         this.factory.DeleteAllTrips();
@@ -23,25 +23,18 @@ public class CreateDraftTests
     public async Task Draft_trip_is_created()
     {
         // Arrange
-        var city = this.factory.Fixture.Create<string>();
-        await this.factory.AddCityAsync(city);
+        var city = factory.Fixture.Create<string>();
+        await factory.AddCityAsync(city);
 
-        this.factory.CoordinatesAgentMock.Setup(x =>
+        factory.CoordinatesAgentMock.Setup(x =>
             x.GetCityByCoordinatesAsync(It.IsAny<Coordinates>(), It.IsAny<CancellationToken>()))
                 .ReturnsAsync(city);
 
-        this.factory.CoordinatesAgentMock.Setup(x =>
+        factory.CoordinatesAgentMock.Setup(x =>
             x.GetLocationByCoordinatesAsync(It.IsAny<Coordinates>(), It.IsAny<CancellationToken>()))
-                .ReturnsAsync(this.factory.Fixture.Create<string>());
+                .ReturnsAsync(factory.Fixture.Create<string>());
 
-        CreateDraftRequest draftRequest = this.factory.Fixture.Build<CreateDraftRequest>()
-            .With(x => x.OriginLatitude, 41.54)
-            .With(x => x.OriginLongitude, 2.10)
-            .With(x => x.DestinationLatitude, 41.38)
-            .With(x => x.DestinationLongitude, 2.17)
-            .Create();
-        
-        Request request = this.factory.Fixture.Build<Request>().With(x => x.CreateDraft, draftRequest).Create();
+        Request request = this.factory.Fixture.CreateDraftTripRequest();
 
         // Act
         Result<CreateDraftResponse> response = await factory.Mediator.Send(request);
---
+++ TripManagement.IntegrationTests/Trips/TripsDataBuilder.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture;
+using TripManagement.Application.Trips.CreateDraft;
+using TripManagement.Contracts.Models;
+
+namespace TripManagement.IntegrationTests.Trips;
+
+public static class TripsDataBuilder
+{
+    public static Request CreateDraftTripRequest(this IFixture fixture)
+    {
+        CreateDraftRequest createDraftRequest = fixture.Build<CreateDraftRequest>()
+            .With(x => x.OriginLatitude, 41.54)
+            .With(x => x.OriginLongitude, 2.10)
+            .With(x => x.DestinationLatitude, 41.38)
+            .With(x => x.DestinationLongitude, 2.17)
+            .Create();
+
+        return fixture.Build<Request>().With(x => x.CreateDraft, createDraftRequest).Create();
+    }
+}
