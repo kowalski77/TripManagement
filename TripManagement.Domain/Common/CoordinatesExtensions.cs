08cdf2ce982adb0dccc7c0d3b7825515c759a3e4
Christian Aranda <christian.arandadiaz@ypsomed.com>
2022-12-11 21:06:03 +0100

    functional way

--- TripManagement.Domain/Common/CoordinatesExtensions.cs
+++ TripManagement.Domain/Common/CoordinatesExtensions.cs
@@ -4,4 +4,16 @@ public static class CoordinatesExtensions
 {
     public static Kilometers DistanceTo(this Coordinates origin, Coordinates destination) =>
         origin.CalculateDistanceTo(destination).ToKilometers();
+
+    private static Distance CalculateDistanceTo(this Coordinates origin, Coordinates destination)
+    {
+        Radians latitude = new Degrees(destination.Latitude - origin.Latitude).ToRadians();
+        Radians longitude = new Degrees(destination.Longitude - origin.Longitude).ToRadians();
+
+        var value = (Math.Sin(latitude.Value / 2) * Math.Sin(latitude.Value / 2)) +
+                (Math.Cos(new Degrees(origin.Latitude).ToRadians().Value) * Math.Cos(new Degrees(destination.Latitude).ToRadians().Value) *
+                Math.Sin(longitude.Value / 2) * Math.Sin(longitude.Value / 2));
+
+        return new Distance(2 * Math.Asin(Math.Sqrt(value)));
+    }
 } 
--- TripManagement.Domain/Common/Models.cs
+++ TripManagement.Domain/Common/Models.cs
@@ -2,10 +2,20 @@
 
 public record struct Radians(double Value);
 
-public record struct Degrees(double Value);
+public record struct Degrees(double Value)
+{
+    public Radians ToRadians() => new(this.Value * (Math.PI / 180));
+}
 
-public record struct Distance(double Value);
+public record struct Kilometers(Distance Distance, double Value);
 
-public record struct Kilometers(double Value);
+public record struct Milles(Distance Distance, double Value);
 
-public record struct Milles(double Value);
+public record struct Distance(double Value)
+{
+    private const int EarthRadius = 6371;
+
+    public Milles ToMilles() => new(this, this.Value * 0.621371);
+
+    public Kilometers ToKilometers() => new(this, (int)(this.Value * EarthRadius));
+}
