950d69ba3aafa6e86e1e87dc0c58a7ef9257f6fb
Christian Aranda <christian.arandadiaz@ypsomed.com>
2022-12-13 19:40:09 +0100

    refactor

--- TripManagement.Domain/Models/Coordinates/Harvesine.cs
+++ TripManagement.Domain/Models/Coordinates/Harvesine.cs
@@ -4,15 +4,13 @@ namespace TripManagement.Domain.Models.Coordinates;
 
 internal static class Harvesine
 {
-    public static Distance CalculateDistanceTo(this Coordinates origin, Coordinates destination)
-    {
-        Radians latitude = new Degrees(destination.Latitude - origin.Latitude).ToRadians();
-        Radians longitude = new Degrees(destination.Longitude - origin.Longitude).ToRadians();
+    public static Distance CalculateDistanceTo(this Coordinates origin, Coordinates destination) =>
+        new(2 * Math.Asin(Math.Sqrt(
+            (Math.Sin(GetRadians(destination.Latitude, origin.Latitude) .Value / 2) * Math.Sin(GetRadians(destination.Latitude, origin.Latitude).Value / 2)) +
+            (Math.Cos(GetRadians(origin.Latitude).Value) * Math.Cos(GetRadians(destination.Latitude).Value) *
+            Math.Sin(GetRadians(destination.Longitude, origin.Longitude).Value / 2) * Math.Sin(GetRadians(destination.Longitude, origin.Longitude).Value / 2)))));
+    
+    private static Radians GetRadians(double a, double b) => new Degrees(a - b).ToRadians();
 
-        var value = (Math.Sin(latitude.Value / 2) * Math.Sin(latitude.Value / 2)) +
-                (Math.Cos(new Degrees(origin.Latitude).ToRadians().Value) * Math.Cos(new Degrees(destination.Latitude).ToRadians().Value) *
-                Math.Sin(longitude.Value / 2) * Math.Sin(longitude.Value / 2));
-
-        return new Distance(2 * Math.Asin(Math.Sqrt(value)));
-    }
+    private static Radians GetRadians(double a) => new Degrees(a).ToRadians();
 }
