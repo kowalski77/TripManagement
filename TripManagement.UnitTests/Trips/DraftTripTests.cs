afe86af1ba73ef6d6e53a1f891607cf197fb28dd
Christian Aranda <christian.arandadiaz@ypsomed.com>
2022-12-25 20:35:47 +0100

    DraftTripTests

---
+++ TripManagement.UnitTests/Trips/DraftTripTests.cs
@@ -0,0 +1,129 @@
+﻿using Arch.SharedKernel.Results;
+using AutoFixture;
+using FluentAssertions;
+using TripManagement.Domain.TripsAggregate;
+using TripManagement.Domain.Types.Locations;
+
+namespace TripManagement.UnitTests.Trips;
+
+public class DraftTripTests
+{
+    private readonly IFixture fixture = new Fixture();
+
+    [Fact]
+    public void Draft_trip_is_created()
+    {
+        // Arrange
+      TripOptions tripOptions =  fixture.Build<TripOptions>()
+            .With(x => x.MaxDistanceBetweenLocations, 100)
+            .With(x => x.MinDistanceBetweenLocations, 1)
+            .With(x => x.AllowedCities, new[] { "Barcelona", "Sabadell" })
+            .Create();
+
+        // Act
+        Result<Trip> result = DraftTrip.Create(
+            Guid.NewGuid(),
+            UserId.CreateInstance(Guid.NewGuid()).Value,
+            fixture.Create<DateTime>(),
+            fixture.CreateOriginLocation(),
+            fixture.CreateDestinationLocation(),
+            tripOptions);
+
+        // Assert
+        result.Success.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Draft_with_city_not_allowed_is_not_created()
+    {
+        // Arrange
+        TripOptions tripOptions = fixture.Build<TripOptions>()
+            .With(x => x.MaxDistanceBetweenLocations, 100)
+            .With(x => x.MinDistanceBetweenLocations, 1)
+            .With(x => x.AllowedCities, new[] { "Terrassa" })
+            .Create();
+
+        // Act
+        Result<Trip> result = DraftTrip.Create(
+            Guid.NewGuid(),
+            UserId.CreateInstance(Guid.NewGuid()).Value,
+            fixture.Create<DateTime>(),
+            fixture.CreateOriginLocation(),
+            fixture.CreateDestinationLocation(),
+            tripOptions);
+
+        // Assert
+        result.Success.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Draft_with_distance_between_locations_greater_than_max_is_not_created()
+    {
+        // Arrange
+        TripOptions tripOptions = fixture.Build<TripOptions>()
+            .With(x => x.MaxDistanceBetweenLocations, 1)
+            .With(x => x.MinDistanceBetweenLocations, 1)
+            .With(x => x.AllowedCities, new[] { "Barcelona", "Sabadell" })
+            .Create();
+
+        // Act
+        Result<Trip> result = DraftTrip.Create(
+            Guid.NewGuid(),
+            UserId.CreateInstance(Guid.NewGuid()).Value,
+            fixture.Create<DateTime>(),
+            fixture.CreateOriginLocation(),
+            fixture.CreateDestinationLocation(),
+            tripOptions);
+
+        // Assert
+        result.Success.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Draft_with_distance_between_locations_lower_than_min_is_not_created()
+    {
+        // Arrange
+        TripOptions tripOptions = fixture.Build<TripOptions>()
+            .With(x => x.MaxDistanceBetweenLocations, 100)
+            .With(x => x.MinDistanceBetweenLocations, 100)
+            .With(x => x.AllowedCities, new[] { "Barcelona", "Sabadell" })
+            .Create();
+
+        // Act
+        Result<Trip> result = DraftTrip.Create(
+            Guid.NewGuid(),
+            UserId.CreateInstance(Guid.NewGuid()).Value,
+            fixture.Create<DateTime>(),
+            fixture.CreateOriginLocation(),
+            fixture.CreateDestinationLocation(),
+            tripOptions);
+
+        // Assert
+        result.Success.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Draft_with_same_origin_and_destination_is_not_created()
+    {
+        // Arrange
+        TripOptions tripOptions = fixture.Build<TripOptions>()
+            .With(x => x.MaxDistanceBetweenLocations, 100)
+            .With(x => x.MinDistanceBetweenLocations, 1)
+            .With(x => x.AllowedCities, new[] { "Barcelona", "Sabadell" })
+            .Create();
+
+        Location location = fixture.CreateOriginLocation();
+
+        // Act
+        Result<Trip> result = DraftTrip.Create(
+            Guid.NewGuid(),
+            UserId.CreateInstance(Guid.NewGuid()).Value,
+            fixture.Create<DateTime>(),
+            location,
+            location,
+            tripOptions);
+
+        // Assert
+        result.Success.Should().BeFalse();
+    }
+}
---
+++ TripManagement.UnitTests/Trips/TripsDataBuilder.cs
@@ -0,0 +1,38 @@
+﻿using AutoFixture;
+using TripManagement.Domain.TripsAggregate;
+using TripManagement.Domain.Types.Coordinates;
+using TripManagement.Domain.Types.Locations;
+
+namespace TripManagement.UnitTests.Trips;
+
+public static class TripsDataBuilder
+{
+    private const double CityOneLatitude = 41.54;
+    private const double CityOneLongitude = 2.10;
+    private const double CityTwoLatitude = 41.38;
+    private const double CityTwoLongitude = 2.17;
+
+    public static Location CreateOriginLocation(this IFixture fixture)
+    {
+        fixture.Customize<City>(x => x.With(y => y.Value, "Sabadell"));
+
+        return Location.Create(
+            Guid.NewGuid(),
+            fixture.Create<Address>(),
+            fixture.Create<City>(),
+            fixture.Create<PlaceId>(),
+            Coordinate.CreateInstance(CityOneLatitude, CityOneLongitude).Value);
+    }
+
+    public static Location CreateDestinationLocation(this IFixture fixture)
+    {
+        fixture.Customize<City>(x => x.With(y => y.Value, "Barcelona"));
+
+        return Location.Create(
+            Guid.NewGuid(),
+            fixture.Create<Address>(),
+            fixture.Create<City>(),
+            fixture.Create<PlaceId>(),
+            Coordinate.CreateInstance(CityTwoLatitude, CityTwoLongitude).Value);
+    }
+}
